197ef1d47ebb384d1bc237a88075229b1145874e
Lee Andrew <31992273+LeeAndrew14@users.noreply.github.com>
2021-07-25 21:46:35 +0800

    Add navbar and toaster!

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Loader from '../components/Loader.js';
+
+import toast from 'react-hot-toast'
+
 export default function Home() {
   return (
     <div className={styles.container}>
-
+       <button onClick={() => toast.success('Hello toast!')}>
+         Toast me!
+       </button>
     </div>
   )
 }
