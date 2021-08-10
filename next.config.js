ab2d21f27efdb27294978a813209c8516714e712
Lee Andrew <31992273+LeeAndrew14@users.noreply.github.com>
2021-08-10 23:45:06 +0800

    Fix firebase config incorrect api key

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  env : {
+    FIREBASE_API_KEY: process.env.apiKey,
+    AUTH_DOMAIN: process.env.authDomain,
+    PROJECT_ID: process.env.projectId,
+    STORAGE_BUCKET: process.env.storageBucket,
+    MESSAGING_SENDER_ID: process.env.messagingSenderId,
+    APP_ID: process.env.appId,
+  }
 }
