dd93b5a1b394b042c18d453e599ba14dab008b4b
Lee Andrew <31992273+LeeAndrew14@users.noreply.github.com>
2021-08-14 17:14:25 +0800

    Add static generation for posts

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,4 +1,47 @@
-export default function PostPage({ }) {
+import { getUserWithUsername, postToJSON } from '@lib/firebase';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve query using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug},
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths : [
+    //   {params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export default function Post({ }) {
   return (
     <main>
     </main>
