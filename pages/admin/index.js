dc283f72cbba95763a93973a9ff7abc300969401
Lee Andrew <31992273+LeeAndrew14@users.noreply.github.com>
2021-08-14 18:21:39 +0800

    Add metatag component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,10 @@
+import Metatags from '@components/Metatags';
+
 export default function AdminPostsPage({ }) {
   return (
     <main>
+     <Metatags title="admin page" />
+     <h1>Edit Post</h1>
     </main>
   )
 }
